acb2dcfd00b62a546d0e486c9949648376c3f85b
pellelito <pelle.lofstrand@gmail.com>
2021-06-16 11:06:24 +0200

    Create gig model

---
+++ GigHuben/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHuben.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public String Name { get; set; }
+    }
+}
---
+++ GigHuben/Models/Gig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHuben.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        [Required]
+        [StringLength(255)]
+        public String Venue { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
